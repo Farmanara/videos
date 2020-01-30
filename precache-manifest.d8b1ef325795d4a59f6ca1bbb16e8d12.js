171ebab7cd7229450b237019812bba3135c62ea8
Amir <farmanara.code@gmail.com>
2020-01-31 01:54:20 +0330

    Updates

---
+++ precache-manifest.d8b1ef325795d4a59f6ca1bbb16e8d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ea1ffcf09f25d446e87a2d9ee2c2bc",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "c2f095175cdcf9e559b3",
+    "url": "/videos/static/css/main.61cfa26d.chunk.css"
+  },
+  {
+    "revision": "044252b6021717c33d8c",
+    "url": "/videos/static/js/2.4440a825.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/videos/static/js/2.4440a825.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2f095175cdcf9e559b3",
+    "url": "/videos/static/js/main.a5f329ea.chunk.js"
+  },
+  {
+    "revision": "df2018c59bd9eb74cc66",
+    "url": "/videos/static/js/runtime-main.ab2e0800.js"
+  }
+]);
